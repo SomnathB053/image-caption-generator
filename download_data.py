a3e294d046e91d00a555fa940ee519464ac052f4
SomnathB053 <111645653+SomnathB053@users.noreply.github.com>
2024-02-04 23:04:54 +0530

    initial

--- download_data.py
+++ download_data.py
@@ -1,12 +1,12 @@
-import urllib
+import urllib.request
 import zipfile
-
+import os
 
 dataset_url = 'https://github.com/jbrownlee/Datasets/releases/download/Flickr8k/Flickr8k_Dataset.zip'
 text_url = 'https://github.com/jbrownlee/Datasets/releases/download/Flickr8k/Flickr8k_text.zip'
 
-extract_dir = '/dataset'
 
+extract_dir = os.getcwd()
 
 def download_unzip_url(url, extract_dir):
   zip_path, _ = urllib.request.urlretrieve(url)
@@ -15,6 +15,6 @@ def download_unzip_url(url, extract_dir):
 
 if __name__ == '__main__':
   print('Downloading dataset - Flicker8k')
-  download_unzip_url(dataset_url)
+  download_unzip_url(dataset_url,os.path.join(extract_dir, 'dataset/images'))
   print('Downloading text - Flicker8k')
-  download_unzip_url(text_url)
+  download_unzip_url(text_url, os.path.join(extract_dir, 'dataset/annot'))
